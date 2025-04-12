d777860a7085eae4a79fed57c66eb9f8bf32227b
Rajat <rajatgsharma29@gmail.com>
2025-04-12 20:06:12 +0530

    Frontend url added to env

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -18,7 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.Frontend_Url!,
     credentials: true               // allow cookies
 }));
 
